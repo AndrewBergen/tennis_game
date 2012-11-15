4b674678396efb18128fe4274fd07c21b0d51fc1
AndrewBergen <andrewjbergen@gmail.com>
2012-11-15 15:36:57 -0600

    Implemented empty game test

--- tennis_game.rb
+++ tennis_game.rb
@@ -3,8 +3,19 @@ require 'test/unit'
 class TennisTest < Test::Unit::TestCase
   
    def test_empty_game
-    assert(false, "This was expected to be true")
+    #Given I have an new tennis game
+    game = TennisGame.new
+    #When you check the score
+    actual = game.score
+    #Then the score should be 0 to 0
+    assert_equal(actual, 0)
    end
    
+end
+
+class TennisGame
+  def score
+    0
+  end
 end
   
