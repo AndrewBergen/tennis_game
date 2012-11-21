92b2563f852ed3c905ee6dd021cd38b76b6bca24
AndrewBergen <andrewjbergen@gmail.com>
2012-11-21 15:48:08 -0600

    Refactorings and cleanup before some redesign

--- tennis_game.rb
+++ tennis_game.rb
@@ -58,13 +58,13 @@ class TennisTest < Test::Unit::TestCase
    
    def test_when_both_players_have_3_or_more_points_and_are_tied
       #Given a tennis game where both players have 3 or more points and are tied
+      #This is a bit ugly. I could use a set_player_points function, but I don't see a use outside of my tests.
       game = TennisGame.new
-      game.new_player_1_point
-      game.new_player_1_point
-      game.new_player_1_point
-      game.new_player_2_point
-      game.new_player_2_point
-      game.new_player_2_point
+      3.times {game.new_player_1_point}
+      
+      3.times do 
+        game.new_player_2_point
+      end
       #When I call score
       actual_p1, actual_p2 = game.score
       #Then it should return 'deuce'
